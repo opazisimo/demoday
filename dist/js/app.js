8f06fba63688576fe31fac038b9429f069ca5adf
opazisimo <suopazo@4talent.cl>
2016-12-01 11:47:41 -0300

    merge task default

--- dist/js/app.js
+++ dist/js/app.js
@@ -27,4 +27,13 @@ $(document).ready(function(){
 		});
 		$("#contenedor").html(templateEstud);
 	});
+
+  $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
+
 });
