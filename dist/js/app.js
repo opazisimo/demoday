f3dcbaafae8eae289852e29e8ac2d67150c0b056
McPoe <maciel.porraz@gmail.com>
2016-12-02 17:41:52 -0500

    icon color and image style

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,32 +1,25 @@
-var template= '<div class="col s12 m12">'+
-				   '<div class="card horizontal">'+
-					   '<div class="card-image">'+
-					   	'<img src="{{imagen}}">'+
-				       '</div>'+
-					    '<div class="card-stacked">'+
-						    '<div class="card-content">'+
-							    '<p><b>Nombre:</b> <span>{{nombre}}</span></p>'+
-							    '<p><b>Edad:</b> <span>{{edad}}</span></p>'+
-							    '<p><b>Nacionalidad:</b> <span>{{nacionalidad}}</span></p>'+
-							    '<p><b>Sede:</b> <span>{{sede}}</span></p>'+
-						    '</div>'+
-						    '<div class="card-action">'+
-						    	'<a href="perfil.html" id="enlace"><b>Calificar</b> <i class="tiny material-icons">star</i></a>'+
-						    '</div>'+
-					    '</div>'+
+var template= '<div class="card contiene col s12 m12">'+
+					'<div class="col s5 m3">'+
+					   	'<div class="image">'+
+					   		'<img src="{{imagen}}">'+
+				       	'</div>'+
 				    '</div>'+
+				    '<div class="col s5 m6">'+
+					    '<p class="nomAlum"><span>{{nombre}}</span></p>'+
+					    '<p class="nomEdad"><span>{{edad}}</span></p>'+
+					    '<p class="nomSede"><span>{{sede}}</span></p>'+
+					'</div>'+
+					'<div class="col s2 m3">'+
+						'<div class="vermas" data="{{number}}">'+
+					    	'<a id="enlace"><i class="fa fa-plus-circle fa-3x ic-color" aria-hidden="true"></i></a>'+
+						'</div>'+
+					'</div>'+
 			   '</div>';
 
 var guardarDirigir= function(){
-	var informacion= $(this).prev().html();
-	var src= $(this).parent().prev().children().attr("src");
-	localStorage.setItem("informacion", informacion);
-	localStorage.setItem("src", src);
-	window.location= "perfil.html";
-}
-
-var infoAlumna= localStorage.getItem("informacion");
-var srcAlumna= localStorage.getItem("src");
+	var self = $(this).attr("data");
+	window.location.href= "perfil.html" + "?data=" + self;
+};
 
 $(document).ready(function(){
 	$.get("/info.json", function(response){
@@ -37,10 +30,11 @@ $(document).ready(function(){
 							.replace("{{edad}}", estudiante.edad)
 							.replace("{{nacionalidad}}", estudiante.nacionalidad)
 							.replace("{{sede}}", estudiante.sede)
-              .replace("{{imagen}}", estudiante.foto);
-		});
-		$("#contenedor").html(templateEstud);
+							.replace("{{number}}", i+1)
+                            .replace("{{imagen}}", estudiante.foto);
 	});
+	$("#contenedor").html(templateEstud);
+});
 
   $('.button-collapse').sideNav({
       menuWidth: 300, // Default is 240
@@ -49,9 +43,7 @@ $(document).ready(function(){
       draggable: true // Choose whether you can drag to open on touch screens
     }
   );
-  $("#contenedor").on("click", ".card-action", guardarDirigir);
-  $("#foto").attr("src", srcAlumna);
-  $("#informacion").html(infoAlumna);
+  $("#contenedor").on("click", ".vermas", guardarDirigir);
 });
 
 $(function() {
